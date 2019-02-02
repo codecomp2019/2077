890df5ef1093999435508a821c29e9891f257b61
cabesd <9100535+cabesd@users.noreply.github.com>
2019-02-02 12:08:40 -0800

    'test'

--- app/src/main/java/com/example/csun_compt/MainActivity.java
+++ app/src/main/java/com/example/csun_compt/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.csun_compt;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextToSpeech tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tts=new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+
+            @Override
+            public void onInit(int status) {
+                // TODO Auto-generated method stub
+                if(status == TextToSpeech.SUCCESS){
+                    int result=tts.setLanguage(Locale.US);
+                    if(result==TextToSpeech.LANG_MISSING_DATA ||
+                            result==TextToSpeech.LANG_NOT_SUPPORTED){
+                        Log.e("error", "This Language is not supported");
+                    }
+                    else{
+                        tts.speak("Hello World", TextToSpeech.QUEUE_FLUSH, null);
+                    }
+                }
+                else
+                    Log.e("error", "Initilization Failed!");
+            }
+        });
+
+        //Speaker speaker = new Speaker();
+        //speaker.textToSpeech("Hi");
     }
 }
