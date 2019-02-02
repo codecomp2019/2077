4fd38738cb857da5814e12fd124154ced14c6157
lwhittaker00 <latejah.whittaker.938@my.csun.edu>
2019-02-02 14:25:33 -0800

    Layout (not yet finished with constraints for different phones, but it compiles and runs in emulator)

---
+++ app/src/main/java/com/example/csun_compt/UserSubmissionPopUp.java
@@ -0,0 +1,13 @@
+package com.example.csun_compt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserSubmissionPopUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_submission_pop_up);
+    }
+}
