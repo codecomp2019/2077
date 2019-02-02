ed851853ee2c2ebc89c867fb8abcd71c58bc0d48
Marcello San Agustin <marcello.sanagustin.448@my.csun.edu>
2019-02-02 14:10:52 -0800

    work done

--- app/src/main/java/com/example/csun_compt/MainActivity.java
+++ app/src/main/java/com/example/csun_compt/MainActivity.java
@@ -1,13 +1,122 @@
 package com.example.csun_compt;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Integer[] images = {R.drawable.meme1, R.drawable.meme2, R.drawable.meme3};
+    int currImage = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //imageSwitcher = (ImageSwitcher)findViewById(R.id.imageSwitcher);
+        Button nextButton = (Button) findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(this);
+        Button backButton = (Button) findViewById(R.id.backButton);
+        backButton.setOnClickListener(this);
+        Button moreButton = (Button) findViewById(R.id.moreButton);
+        moreButton.setOnClickListener(this);
+
+        initializeImageSwitcher();
+        setInitialImage();
+        setImageRotateListener();
+
+       /* imageSwitcher.setFactory(new ViewSwitcher.ViewFactory(){
+           ImageView imageView = new ImageView(getApplicationContext());
+           imageView.setScaleType(Image.ScaleType.CENTER_INSIDE);
+           imageView.setLayoutParams(new ImageSwitcher.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                                        ViewGroup.LayoutParams.MATCH_PARENT));
+
+           return imageView;
+        });*/
+    }
+
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.nextButton:
+               nextButton();
+                break;
+            case R.id.backButton:
+              //  backButton();
+                break;
+            case R.id.moreButton:
+             //   moreButton();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    public void nextButton() {
+
+    }
+
+    private void initializeImageSwitcher() {
+        final ImageSwitcher imageSwitcher = (ImageSwitcher) findViewById(R.id.imageSwitcher);
+        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(MainActivity.this);
+                return imageView;
+            }
+        });
+        imageSwitcher.setInAnimation(AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left));
+        imageSwitcher.setOutAnimation(AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right));
+    }
+
+    private void setImageRotateListener() {
+        final Button rotatebutton = (Button) findViewById(R.id.nextButton);
+        rotatebutton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                currImage++;
+                if (currImage == 3) {
+                    currImage = 0;
+                }
+                setCurrentImage();
+            }
+        });
+    }
+
+    private void setInitialImage() {
+        setCurrentImage();
+    }
+
+    private void setCurrentImage() {
+        final ImageSwitcher imageSwitcher = (ImageSwitcher) findViewById(R.id.imageSwitcher);
+        imageSwitcher.setImageResource(images[currImage]);
     }
 }
+
+  /*  public void nextButton(){
+        if(count < images.length -1){
+            count++;
+            imageSwitcher.setImageResource(images[count]);
+        }
+    }
+
+    public void backButton(){
+        if(0 < count){
+            count--;
+            imageSwitcher.setImageResource(image[count]);
+        }
+    }*/
+
+
+
---
+++ app/src/main/java/com/example/csun_compt/listeners.java
@@ -0,0 +1,16 @@
+package com.example.csun_compt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class listeners extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listeners);
+    }
+
+    
+
+}
